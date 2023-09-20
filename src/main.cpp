ed06d10e4a1f0343d2e323009189cfa18166ac7e
Thomas Kriechbaumer <thomas@kriechbaumer.name>
2023-09-20 15:49:32 +0200

    migrate to Arduino-provided NTP integration

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,13 @@
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-#include <WiFiClientSecure.h>
-#include <SPI.h>
+#include <ESP8266WiFi.h>
 #include <SDFS.h>
-#include <WiFiUdp.h>
-#include <NTPClient.h>
+#include <SPI.h>
+#include <SoftwareSerial.h>
 #include <Ticker.h>
 #include <TinyGPS++.h>
-#include <SoftwareSerial.h>
+#include <WiFiClientSecure.h>
+#include <time.h>
 
 #include "customizations.h"
 #include "logging.h"
@@ -38,8 +37,8 @@ void setup();
 void init_gps_file();
 void upload_gps_file();
 void init_assistnow();
-bool download_online_blob(time_t epoch_time);
-bool download_offline_blob(time_t epoch_time);
+bool download_online_blob(time_t epoch_time, struct tm *time_info);
+bool download_offline_blob(time_t epoch_time, struct tm *time_info);
 void load_assistnow_blob(String filename);
 bool connect_wifi();
 void check_serial_commands();
@@ -55,9 +54,7 @@ Ticker output_ticker;
 SoftwareSerial uart_gps;
 TinyGPSPlus gps;
 double old_lat;
-double old_lng;
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP);
+double old_lon;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -163,10 +160,9 @@ bool connect_wifi() {
   log_println(WiFi.localIP().toString());
   Serial.flush();
 
-  timeClient.begin();
-  timeClient.forceUpdate();
-  delay(1000);
-  log_printfln("NTP updated:     %lu", timeClient.getEpochTime());
+    configTime(0, 0, "pool.ntp.org", "time.nist.gov");
+    delay(2000);
+    log_printfln("NTP epoch: %ld", time(nullptr));
 
   return true;
 }
@@ -181,8 +177,7 @@ void loop() {
     }
     gps.encode(b);
 
-    if (!assistnow_initialized && gps.date.isValid() && gps.time.isValid())
-    {
+        if (!assistnow_initialized) {
       init_assistnow();
     }
 
@@ -239,8 +234,8 @@ void persist_location_record() {
   no_location_update_counter = 0;
 
   double new_lat = gps.location.lat();
-  double new_lng = gps.location.lng();
-  double distance_travelled = gps.distanceBetween(old_lat, old_lng, new_lat, new_lng);
+    double new_lon = gps.location.lng();
+    double distance_travelled = gps.distanceBetween(old_lat, old_lon, new_lat, new_lon);
     if (distance_travelled < 5.0) {
     log_printfln(TIMESTAMP_FORMAT ": Only travelled %.2f meters since last update - skipping.",
                  TIMESTAMP_ARGS,
@@ -248,13 +243,13 @@ void persist_location_record() {
     return;
     } else {
     old_lat = new_lat;
-    old_lng = new_lng;
+        old_lon = new_lon;
   }
 
   char record[80];
-  sprintf(record, TIMESTAMP_FORMAT ";%.6f;%.6f;%.2f;%.2f;%d",
+    snprintf(record, sizeof(record), TIMESTAMP_FORMAT ";%.6f;%.6f;%.2f;%.2f;%d",
           TIMESTAMP_ARGS,
-          new_lat, new_lng, gps.altitude.meters(), gps.speed.mps(), gps.satellites.value());
+             new_lat, new_lon, gps.altitude.meters(), gps.speed.mps(), gps.satellites.value());
 
   File gpsFile = SDFS.open(GPS_FILENAME, "a");
   gpsFile.println(record);
@@ -307,7 +302,7 @@ void upload_gps_file() {
     log_printfln("Upload failed with unexpected %d HTTP status code.", http_code);
   }
 
-  WiFi.disconnect(true);
+    return;
 }
 
 bool download_file_to_sd(char *url, char *filename) {
@@ -397,8 +392,7 @@ void cleanup_outdated_assistnow_blobs(time_t epoch_time) {
   }
 }
 
-String find_valid_assistnow_blob(time_t epoch_time)
-{
+String find_valid_assistnow_blob() {
   String best_match = "";
   time_t best_epoch = 0;
 
@@ -439,39 +433,31 @@ void init_assistnow() {
     return;
   }
 
-  struct tm t;
-  t.tm_year = gps.date.year() - 1900;
-  t.tm_mon = gps.date.month() - 1;
-  t.tm_mday = gps.date.day();
-  t.tm_hour = gps.time.hour();
-  t.tm_min = gps.time.minute();
-  t.tm_sec = gps.time.second();
-  time_t epoch_time = mktime(&t);
-  log_printfln("GPS module time: %lld - " TIMESTAMP_FORMAT, epoch_time, TIMESTAMP_ARGS);
+    time_t epoch_time = time(nullptr);
+    struct tm *time_info = gmtime(&epoch_time);
 
   cleanup_outdated_assistnow_blobs(epoch_time);
 
   // download OFFLINE data first
-  download_offline_blob(epoch_time);
+    download_offline_blob(epoch_time, time_info);
   // download ONLINE data and immediately upload it as the most recent blob
-  download_online_blob(epoch_time);
-  String filename = find_valid_assistnow_blob(epoch_time);
+    download_online_blob(epoch_time, time_info);
+    String filename = find_valid_assistnow_blob();
   load_assistnow_blob(filename);
 
   WiFi.disconnect(true);
 }
 
-bool download_online_blob(time_t epoch_time)
-{
+bool download_online_blob(time_t epoch_time, struct tm *time_info) {
   char online_filename[60];
   snprintf(online_filename, sizeof(online_filename), "ASSISTNOW-ONLINE-%llu-%d-%02d-%02dT%02d-%02d-%02d.bin",
            epoch_time,
-           gps.date.year(),
-           gps.date.month(),
-           gps.date.day(),
-           gps.time.hour(),
-           gps.time.minute(),
-           gps.time.second());
+             time_info->tm_year + 1900,
+             time_info->tm_mon + 1,
+             time_info->tm_mday,
+             time_info->tm_hour,
+             time_info->tm_min,
+             time_info->tm_sec);
   char url[200];
   snprintf(url, sizeof(url), "%s?token=%s;gnss=gps,glo,gal;datatype=eph,alm,aux,pos;lat=%.6f;lon=%.6f;alt=%.2f;pacc=50000;latency=1",
            ASSISTNOW_ONLINE_BASE_URL,
@@ -482,17 +468,16 @@ bool download_online_blob(time_t epoch_time)
   return download_file_to_sd(url, online_filename);
 }
 
-bool download_offline_blob(time_t epoch_time)
-{
+bool download_offline_blob(time_t epoch_time, struct tm *time_info) {
   char offline_filename[60];
   snprintf(offline_filename, sizeof(offline_filename), "ASSISTNOW-OFFLINE-%llu-%d-%02d-%02dT%02d-%02d-%02d.bin",
            epoch_time,
-           gps.date.year(),
-           gps.date.month(),
-           gps.date.day(),
-           gps.time.hour(),
-           gps.time.minute(),
-           gps.time.second());
+             time_info->tm_year + 1900,
+             time_info->tm_mon + 1,
+             time_info->tm_mday,
+             time_info->tm_hour,
+             time_info->tm_min,
+             time_info->tm_sec);
 
   // adding the almanac parameter causes an error, see https://portal.u-blox.com/s/question/0D52p00009in3mk/offline-assistnow-almanac-url
   char url[170];
